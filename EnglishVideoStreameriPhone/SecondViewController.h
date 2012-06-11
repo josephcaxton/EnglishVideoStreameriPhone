a767a0cf5db1314a48269555740eeb68afd47e8b
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-06-11 11:15:35 +0100

    Initial Commit

---
+++ EnglishVideoStreameriPhone/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  EnglishVideoStreameriPhone
+//
+//  Created by Joseph caxton-Idowu on 11/06/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ EnglishVideoStreameriPhone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EnglishVideoStreameriPhone
+//
+//  Created by Joseph caxton-Idowu on 11/06/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
